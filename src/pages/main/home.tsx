79e9fd54f7be9b9f020103569eece95c0c3b06c0
Ramazza <01ramazza@gmail.com>
2026-05-12 17:28:36 -0300

    First commit

---
+++ src/pages/main/home.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles';
+
+function Home() {
+
+   return(
+       <S.HomeWrapper>
+        <S.Header>
+            <S.HeaderTitle>Rename</S.HeaderTitle>
+        </S.Header>
+       </S.HomeWrapper>
+   );
+}
+
+export default Home;
---
+++ src/pages/main/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HomeWrapper = styled.div`
+    height: 100vh;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10vh;
+    background-color: #313131;
+`;
+
+export const HeaderTitle = styled.h1`
+    color: #909090;
+`;
